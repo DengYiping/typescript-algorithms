38a3bbae937883225e9afb96f9a93d4d0c9bc70d
Yiping Deng <yiping.deng.98@gmail.com>
2019-03-11 23:11:08 +0100

    first build

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = '--libraryname--'
+const libraryName = 'typescript-algorithms'
 
 export default {
   input: `src/${libraryName}.ts`,
